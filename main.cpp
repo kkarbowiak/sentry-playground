e5556fb18ab6519ed4f53f91a6f95d4e1de66922
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2022-07-21 16:29:13 +0200

    Add proper DSN

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 int main(int argc, char * argv[])
 {
     sentry_options_t *options = sentry_options_new();
-    sentry_options_set_dsn(options, "https://YOUR_KEY@oORG_ID.ingest.sentry.io/PROJECT_ID");
+    sentry_options_set_dsn(options, "https://2eb500f498af4a66b22ab4873e5a4b38@o1326645.ingest.sentry.io/6587187");
     sentry_init(options);
 
     for (auto i = 0; i < argc; ++i)
