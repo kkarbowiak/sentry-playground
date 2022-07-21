dccf5043de60d8b493c6f3a6c21a1424bdeaac66
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2022-07-21 12:36:44 +0200

    Add dummy Sentry init code

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
+#include "sentry.h"
 #include <iostream>
+
 
 
 int main(int argc, char * argv[])
 {
+    sentry_options_t *options = sentry_options_new();
+    sentry_options_set_dsn(options, "https://YOUR_KEY@oORG_ID.ingest.sentry.io/PROJECT_ID");
+    sentry_init(options);
+
     for (auto i = 0; i < argc; ++i)
     {
         std::cout << argv[i] << '\n';
     }
+
+    sentry_close();
 
     return 0;
 }
